841fe85a8d82cef25c76ff808a5c68c779f7b2bf
Gopi Sumanth Bhaskar <gopi@semanticwebindia.com>
2021-01-24 13:06:12 +0530

    Update settings.py

--- GVA/settings.py
+++ GVA/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'w2#+wb(#1ssk!7xx^46=4n$%tm@l^7nhw_m=9ce*c#5ljhkjl7'
+SECRET_KEY = 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
